06e1cc878e78e806af7b052f318bd85ecb1451ac
CPH <hypery11@gmail.com>
2015-01-24 13:08:27 +0000

    YO

--- crawler.php
+++ crawler.php
@@ -25,15 +25,15 @@ flush();
 $url='http://www.319.com.tw/store/show/';
 
 $arr=explode("\n",strip_tags(file_get_contents($url.$num)));
-echo $num."  =  ".trim($arr[219])."<br>";
+echo $num."  =  ".trim($arr[228])."<br>";
 
 if(trim($arr[219])!="")
 	{
-		$name=trim($arr[219]);
-		$clas=trim($arr[245]);
-		$city=trim($arr[249]);
-		$address=trim($arr[253]);
-		$phone=trim($arr[257]);
+		$name=trim($arr[228]);
+		$clas=trim($arr[254]);
+		$city=trim($arr[258]);
+		$address=trim($arr[262]);
+		$phone=trim($arr[266]);
 
 		record($num,$name,$clas,$city,$address,$phone);
 	}
@@ -42,12 +42,9 @@ if(trim($arr[219])!="")
 /*
 foreach($arr as $key=>$val)
 	echo $key.$val."<br>";
-219=>名稱  245=>類別  249=>縣市鄉鎮  253=>地址
-257=>電話  261=>營業時間  263=>價格  283=>網趾
 
 num range=3325 to 9532
 */
-//echo trim($arr[29])."<br><br>".trim($arr[35]);
 }
 
 function record($num,$name,$clas,$city,$address,$phone)
